de0eed35f625d2f1ecdf264d70a9757b8089ac95
4rank <frankensteinn.v@gmail.com>
2020-11-07 01:10:38 +0300

    just something

---
+++ src/by/it/fedorinhyk/practice/Practice.java
@@ -0,0 +1,26 @@
+package by.it.fedorinhyk.practice;
+
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Веди цифру от 0 до 5!");
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int answer= (int) (0+Math.random()*5);
+        if (a==answer){
+            System.out.println("Молодец!");
+            System.out.println("Самоуничтожение через...");
+            Thread.sleep(1500);
+            for (int i = 0; i < 10; i++) {
+                System.out.println(10-i+"...");
+                Thread.sleep(1000);
+            }
+            Thread.sleep(1550);
+            System.out.println("БАБАХ!!!");
+        }
+        if (a!=answer){
+            System.out.println("Правильный ответ: "+answer);
+        }
+    }
+}
---
+++ src/by/it/fedorinhyk/practice/Practice2.java
@@ -0,0 +1,44 @@
+package by.it.fedorinhyk.practice;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Practice2 {
+    public static void main(String[] args) throws InterruptedException {
+        Date mom=new Date();
+        Calendar time = Calendar.getInstance();
+        mom=time.getTime();
+        System.out.println("Hi!!!");
+        Thread.sleep(1500);
+        System.out.println("What time is it???");
+        Thread.sleep(1000);
+        System.out.println(mom+" Hmmmm.....");
+        Thread.sleep(2000);
+        System.out.println("It's time to do something!!!");
+        deal();
+    }
+    private static void deal() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Write number between 0 and 5");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int answer = (int) (0 + Math.random() * 5);
+        if (a == answer) {
+            Thread.sleep(1000);
+            System.out.println("You're so lucky!");
+        }
+        else if (a != answer && a >= 0 && a <= 5) {
+            Thread.sleep(1000);
+            System.out.println("Wrong!!!");
+            Thread.sleep(1500);
+            System.out.println("The right answer is: " + answer);
+        }
+        else {
+            Thread.sleep(1000);
+            System.out.println("Oh my god...");
+            Thread.sleep(1000);
+            System.out.println("I say BETWEEN 0 AND 5!");
+        }
+    }
+}
