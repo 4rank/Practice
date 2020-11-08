c466e5ae28259b80fadab20204175bffbae7c4a8
4rank <frankensteinn.v@gmail.com>
2020-11-08 17:19:46 +0300

    work

--- src/by/it/fedorinhyk/practice/covid.java
+++ src/by/it/fedorinhyk/practice/covid.java
@@ -2,16 +2,13 @@ package by.it.fedorinhyk.practice;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Scanner;
 
 public class covid {
     public static void main(String[] args) throws InterruptedException {
-        for (;;) {
-            Calendar calendar = Calendar.getInstance();
-            Date moment = calendar.getTime();
+        for (int i = 8; i <31 ; i++) {
             int des = 950 + (int) (Math.random() * 49);
             int death = 1 + (int) (Math.random() * 4);
-            System.out.printf("Заболели:%s Умерли:%s Время: %s", des, death, moment);
+            System.out.printf("Заболели:%s Умерли:%s %2d Ноября", des, death, i);
             System.out.println(" ");
             Thread.sleep(2000);
         }
