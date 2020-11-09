a46394141e2ecd0447ac22953c8b85c2d2e5a021
4rank <frankensteinn.v@gmail.com>
2020-11-09 16:00:32 +0300

    work

--- src/by/it/fedorinhyk/practice/covid.java
+++ src/by/it/fedorinhyk/practice/covid.java
@@ -8,12 +8,11 @@ public class covid {
         for (int i = 9; i < 31; i++) {
             int des = 950 + (int) (Math.random() * 49);
             int death = 1 + (int) (Math.random() * 4);
-            int sum= (int) total.stream().mapToDouble(a->a).sum();
             total.add(des);
-            System.out.printf(" %2d Ноября- Заболели:%s Умерли:%s Всего болеет:%s", i, des, death,sum);
+            int sum= (int) total.stream().mapToDouble(a->a).sum();
+            System.out.printf(" %2d Ноября- Заболели:%s Умерли:%s Всего заболело:%s", i, des, death,sum);
             System.out.println(" ");
             Thread.sleep(2000);
         }
     }
 }
-
