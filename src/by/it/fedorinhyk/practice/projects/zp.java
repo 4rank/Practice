8af704cdc048f263495874741b32510079ce9d89
4rank <frankensteinn.v@gmail.com>
2020-11-16 21:03:13 +0300

    work

--- src/by/it/fedorinhyk/practice/projects/zp.java
+++ src/by/it/fedorinhyk/practice/projects/zp.java
@@ -10,7 +10,32 @@ public class zp {
         System.out.println("Введите фамилию сотрудника");
         String fam=sc.nextLine();
         for (int i = 1; i <=12 ; i++) {
-            System.out.println("Введите зарплату сотрудника за "+i+" месяц");
+            switch (i) {
+                case 1:
+                System.out.println("Введите зарплату сотрудника за январь"); break;
+                case 2:
+                    System.out.println("Введите зарплату сотрудника за февраль"); break;
+                case 3:
+                    System.out.println("Введите зарплату сотрудника за март"); break;
+                case 4:
+                    System.out.println("Введите зарплату сотрудника за апрель"); break;
+                case 5:
+                    System.out.println("Введите зарплату сотрудника за май"); break;
+                case 6:
+                    System.out.println("Введите зарплату сотрудника за июнь"); break;
+                case 7:
+                    System.out.println("Введите зарплату сотрудника за июль"); break;
+                case 8:
+                    System.out.println("Введите зарплату сотрудника за август"); break;
+                case 9:
+                    System.out.println("Введите зарплату сотрудника за сентябрь"); break;
+                case 10:
+                    System.out.println("Введите зарплату сотрудника за октябрь"); break;
+                case 11:
+                    System.out.println("Введите зарплату сотрудника за ноябрь"); break;
+                case 12:
+                    System.out.println("Введите зарплату сотрудника за декабрь"); break;
+            }
             int zp = sc.nextInt();
             sum.add(zp);
         }
