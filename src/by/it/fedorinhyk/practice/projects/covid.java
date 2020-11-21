6be91b543ee7c8035e94fc74ec56caf59899bba6
4rank <frankensteinn.v@gmail.com>
2020-11-21 13:01:34 +0300

    work

--- src/by/it/fedorinhyk/practice/projects/covid.java
+++ src/by/it/fedorinhyk/practice/projects/covid.java
@@ -8,31 +8,38 @@ import java.util.Scanner;
 
 public class covid {
     public static void main(String[] args) throws InterruptedException {
-        Scanner sc = new Scanner(System.in);
         System.out.println("Статистика covid-19 для минздрава на 30 дней.");
         Thread.sleep(1000);
-        System.out.println("Введите число заболевших и умерших."+"\n"+"На данный момент заболело:");
-        int bol=sc.nextInt();
-        System.out.println("На данный момент умерло:");
-        int dead=sc.nextInt();
-        ArrayList<Integer> total = new ArrayList<>();
-        Calendar time=Calendar.getInstance();
-        Locale rus=new Locale("ru","RU");
-        SimpleDateFormat date = new  SimpleDateFormat("dd MMMM", rus);
-        final ArrayList<Integer> totalDead = new ArrayList<>();
-        for (int i = 1; i <= 31; i++) {
-            time.add(Calendar.DAY_OF_MONTH,+i);
-            int des = 950 + (int) (Math.random() * 250);
-            int death = 2 + (int) (Math.random() * 6);
-            total.add(des);
-            int sum=total.stream().mapToInt(a->a).sum();
-            totalDead.add(death);
-            int sumDead=totalDead.stream().mapToInt(b->b).sum();
-            System.out.printf("%s - Заболели:%s Умерли:%s",date.format(time.getTime()) , des, death+"\n");
-            System.out.printf(" Всего заболело:%s Всего умерло:%s", sum+bol, sumDead+dead+"\n");
-            System.out.println(" ");
-            Thread.sleep(2000);
-            time.add(Calendar.DAY_OF_MONTH,-i);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Введите число заболевших и умерших."+"\n"+"На данный момент заболело:");
+            if (sc.hasNextInt()) {
+                int bol = sc.nextInt();
+                System.out.println("На данный момент умерло:");
+                if (sc.hasNextInt()){
+                int dead = sc.nextInt();
+                ArrayList<Integer> total = new ArrayList<>();
+                Calendar time = Calendar.getInstance();
+                Locale rus = new Locale("ru", "RU");
+                SimpleDateFormat date = new SimpleDateFormat("dd MMMM", rus);
+                final ArrayList<Integer> totalDead = new ArrayList<>();
+                for (int i = 1; i <= 31; i++) {
+                    time.add(Calendar.DAY_OF_MONTH, +i);
+                    int des = 950 + (int) (Math.random() * 250);
+                    int death = 2 + (int) (Math.random() * 6);
+                    total.add(des);
+                    int sum = total.stream().mapToInt(a -> a).sum();
+                    totalDead.add(death);
+                    int sumDead = totalDead.stream().mapToInt(b -> b).sum();
+                    System.out.printf("%s - Заболели:%s Умерли:%s", date.format(time.getTime()), des, death + "\n");
+                    System.out.printf(" Всего заболело:%s Всего умерло:%s", sum + bol, sumDead + dead + "\n");
+                    System.out.println(" ");
+                    Thread.sleep(2000);
+                    time.add(Calendar.DAY_OF_MONTH, -i);
+                }
+            } else {
+                    System.out.println("Введите целое число.");
+                }
+            }
         }
     }
 }
--- src/by/it/fedorinhyk/practice/projects/zp.java
+++ src/by/it/fedorinhyk/practice/projects/zp.java
@@ -5,43 +5,78 @@ import java.util.Scanner;
 
 public class zp {
     public static void main(String[] args) throws InterruptedException {
-        Scanner sc = new Scanner(System.in);
-        final ArrayList<Integer> sum = new ArrayList<>();
-        System.out.println("Введите фамилию сотрудника");
-        String fam=sc.nextLine();
-        for (int i = 1; i <=12 ; i++) {
-            switch (i) {
-                case 1:
-                System.out.println("Введите зарплату сотрудника за январь"); break;
-                case 2:
-                    System.out.println("Введите зарплату сотрудника за февраль"); break;
-                case 3:
-                    System.out.println("Введите зарплату сотрудника за март"); break;
-                case 4:
-                    System.out.println("Введите зарплату сотрудника за апрель"); break;
-                case 5:
-                    System.out.println("Введите зарплату сотрудника за май"); break;
-                case 6:
-                    System.out.println("Введите зарплату сотрудника за июнь"); break;
-                case 7:
-                    System.out.println("Введите зарплату сотрудника за июль"); break;
-                case 8:
-                    System.out.println("Введите зарплату сотрудника за август"); break;
-                case 9:
-                    System.out.println("Введите зарплату сотрудника за сентябрь"); break;
-                case 10:
-                    System.out.println("Введите зарплату сотрудника за октябрь"); break;
-                case 11:
-                    System.out.println("Введите зарплату сотрудника за ноябрь"); break;
-                case 12:
-                    System.out.println("Введите зарплату сотрудника за декабрь"); break;
+        try (Scanner sc = new Scanner(System.in)) {
+            final ArrayList<Double> sum = new ArrayList<>();
+            final ArrayList<Double> sumPercent = new ArrayList<>();
+            System.out.println("Введите фамилию сотрудника");
+            String fam = sc.nextLine();
+            for (int i = 1; i <= 12; i++) {
+                switch (i) {
+                    case 1:
+                        System.out.println("Введите зарплату сотрудника за январь в рублях");
+                        break;
+                    case 2:
+                        System.out.println("Введите зарплату сотрудника за февраль в рублях");
+                        break;
+                    case 3:
+                        System.out.println("Введите зарплату сотрудника за март в рублях");
+                        break;
+                    case 4:
+                        System.out.println("Введите зарплату сотрудника за апрель в рублях");
+                        break;
+                    case 5:
+                        System.out.println("Введите зарплату сотрудника за май в рублях");
+                        break;
+                    case 6:
+                        System.out.println("Введите зарплату сотрудника за июнь в рублях");
+                        break;
+                    case 7:
+                        System.out.println("Введите зарплату сотрудника за июль в рублях");
+                        break;
+                    case 8:
+                        System.out.println("Введите зарплату сотрудника за август в рублях");
+                        break;
+                    case 9:
+                        System.out.println("Введите зарплату сотрудника за сентябрь в рублях");
+                        break;
+                    case 10:
+                        System.out.println("Введите зарплату сотрудника за октябрь в рублях");
+                        break;
+                    case 11:
+                        System.out.println("Введите зарплату сотрудника за ноябрь в рублях");
+                        break;
+                    case 12:
+                        System.out.println("Введите зарплату сотрудника за декабрь в рублях");
+                        break;
+                }
+                if (sc.hasNextDouble()) {
+                    double zp = sc.nextDouble();
+                    sum.add(zp);
+                    sumPercent.add(zp);
+                } else {
+                    System.out.println("В зарплаты вводите числа.");
+                    System.exit(0);
+                }
             }
-            int zp = sc.nextInt();
-            sum.add(zp);
+            double sumAll = sum.stream().mapToDouble(a -> a).sum();
+            System.out.println("Введите процент налога числом.");
+            if (sc.hasNextDouble()) {
+                double percent = sc.nextDouble();
+                double fin = sumAll * ((100 - percent) / 100);
+                for (int i = 0; i <sumPercent.size() ; i++) {
+                    int j= (int) (sumPercent.get(i)* ((100 - percent) / 100));
+                    sumPercent.set(i, (double) j);
+                }
+                Thread.sleep(500);
+                System.out.println(fam + ": зарпаты по месяцам-" + sum);
+                System.out.println("Подоходный налог: " + percent);
+                System.out.println(fam + ": Зарплаты по месяцам с вычетом налога-" + sumPercent);
+                System.out.printf("Всего за год: %.2f",fin);
+            }
+             else {
+                System.out.println("Введите процент чилом без знака %");
+                System.exit(0);
+            }
         }
-        int sumAll=sum.stream().mapToInt(a->a).sum();
-        Thread.sleep(1000);
-        System.out.println(fam+": зарпаты по месяцам-"+sum);
-        System.out.println("Всего за год: "+sumAll);
     }
 }
